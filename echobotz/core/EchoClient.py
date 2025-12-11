022a0e1befbd6e52912fc490d88cf6e48ccd2ed7
TheZake <patrioremrej@gmail.com>
2025-12-11 17:50:43 +0530

    2

---
+++ echobotz/core/EchoClient.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+from config import Config
+from .. import LOGGER
+
+EchoBot = Client(
+    "EchoBotz",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    workers=100,
+    parse_mode=ParseMode.HTML,
+)
